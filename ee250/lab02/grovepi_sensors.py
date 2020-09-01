72fe805a83c4040959671fecdc335b22232089c7
FlameDragon0 <chenjosh@usc.edu>
2020-09-01 07:42:14 -0700

    Changing text display without refreshing

--- ee250/lab02/grovepi_sensors.py
+++ ee250/lab02/grovepi_sensors.py
@@ -52,16 +52,18 @@ def setRGB(r,g,b):
 # send command to display (no need for external use) [Function copied from grove_rgb_lcd.py]   
 def textCommand(cmd):
     bus.write_byte_data(DISPLAY_TEXT_ADDR,0x80,cmd)
-
-# set display text \n for second line(or auto wrap) [Function copied from grove_rgb_lcd.py]    
-def setText(text):
-    textCommand(0x01) # clear display
+  
+#Update the display without erasing the display [Function copied from grove_rgb_lcd.py]
+def setText_norefresh(text):
+    textCommand(0x02) # return home
     time.sleep(.05)
     textCommand(0x08 | 0x04) # display on, no cursor
     textCommand(0x28) # 2 lines
     time.sleep(.05)
     count = 0
     row = 0
+    while len(text) < 32: #clears the rest of the screen
+        text += ' '
     for c in text:
         if c == '\n' or count == 16:
             count = 0
@@ -115,5 +117,5 @@ if __name__ == '__main__':
           printText += "\n"
         printText += str(int(ultrasonic_value)) + "cm"
 
-        setText(printText) # Prints text on LCD
+        setText_norefresh(printText) # Prints text on LCD without refreshing
 
