bba0fbadc9a5c2071580790c4e0ea0d485bab63b
FlameDragon0 <chenjosh@usc.edu>
2020-11-08 17:48:18 -0800

    Added logic for Ultrasonic Sensor

--- ee250/Final Project/rpi_main.py
+++ ee250/Final Project/rpi_main.py
@@ -6,33 +6,28 @@ sys.path.append('../../Software/Python/grove_rgb_lcd/')
 import grovepi
 import grove_rgb_lcd
 
+time = 0
+time_blocked = 0
+people = 0
+mood = 0
 buzzer_port = 8 # D8
 ultrasonic_port = 7 # D7
 led_port = 3
 button_port = 0 # D0
 
 
-def mode(client, userdata, mode_message):
-    mode_payload = str(mode_message.payload, "utf-8")
-    if mode_payload == "0":
-        print(0)
-    elif mode_payload == "1":
-        print(1)
-    elif mode_payload == "2":
-        print(2)
-    elif mode_payload == "3":
-        print(3)
-    elif mode_payload == "4":
-        print(4)
+def mode(client, userdata, mood_message):
+    mood_payload = str(mood_message.payload, "utf-8")
+    mood = int(mood_payload)
 
 def on_connect(client, userdata, flags, rc):
     print("Connected to server (i.e., broker) with result code "+str(rc))
 
-    #subscribe to topics of interest here
+    # Subscribed topics
     client.subscribe("chenjosh/customMode")
     client.message_callback_add("chenjosh/customMode", mode)
 
-#Default message callback. Please use custom callbacks.
+# Default message callback.
 def on_message(client, userdata, msg):
     print("on_message: " + msg.topic + " " + str(msg.payload, "utf-8"))
 
@@ -46,4 +41,24 @@ if __name__ == '__main__':
     client.loop_start()
 
 while True:
-    time.sleep(1)
+    if time = 100: # Publish every 0.05 x 100 = 5 seconds
+        time = 0
+        client.publish("chenjosh/people", str(people))
+        client.publish("chenjosh/mood", str(mood))
+        #publish
+    
+    grove_rgb_lcd.setText_norefresh("People: " + people + "\nNo custom mood")
+
+    ultrasonic_value = grovepi.ultrasonicRead(ultrasonic_port)
+
+    if int(ultrasonic_value) < 70: # If something is less than 70cm away from the ultrasonic ranger, then something must be going through the door
+        time_blocked += 1
+    elif time_blocked > 2: # If something blocks the doorway for 0.2s or more, then we assume that a person did go through
+        people += 1
+    else:
+        time_blocked = 0 # If not, then we don't count as someone went through the doorway.
+    
+    time += 1
+    time.sleep(0.05) # Sleep for 0.1 seconds
+
+
